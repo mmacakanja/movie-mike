bcfac8e6df85ae637f2d096a0ed40e618a008228
mmacakanja <mmacakanja@gmail.com>
2026-09-10 20:13:01 -0500

    Update columnist.js
    added Bella's first column

--- columnist.js
+++ columnist.js
@@ -7,10 +7,25 @@
 window.VISITING_COLUMNIST = {
   published: false,
   sectionHeadline: "A different seat in the theater",
-  sectionDek: "Occasional guest essays, alternate verdicts and friendly dissent.",
-  author: "Guest name",
-  date: "Month Day, Year",
-  title: "Column title",
+  sectionDek: "Hello and welcome to AMC and the city. 
+
+As a premier AMC a-list stubs member, as well as the daughter with the most free time of “mike” (of “mike’s movie ratings dot com”) it is time I embrace my full potential which deserves to take precedence over my job: guest columnist on my dad’s movie website.
+
+However, my presence here is tricky, and slightly dubious, because I have deep issues with the rating system he has employed. For example, the numerical attachment to a movie’s emotion. Emotional resonance FOR HIM is not directly tied to being emotional in the way you might think. “Mike” does not always look for sentimentality or sadness, but just the evocation of a strong feeling, positive or negative. For example, Hamnet was not a 7 (a mistake). Point made. Anyways, I am not only here to complain, but to talk to y’all about what movies are like down here over yonder in the south.
+
+My plan for this column, because I am not feeling creative or specifically insightful, is to write what I’ve watched this week and my brief feelings about the pictures (follow me on letterboxed plz). Despite this meandering intro, I am not in the mood to write that much, so these will be efficient yet funny, charming, and obviously, correct. Numbers be damned, my opinions are all feelings and no data! Spread sheets don’t belong in my backyard or my theaters!! “Rebellion against tyrants is obedience to God” -Benjamin Franklin.
+
+1) Jaws: scary, hate sharks, but love when classic movies are good. Glad sharks aren’t real. Reminded me of Shark Tales (2004). Shout out Bruce.
+2) By Any Means: I love revenge and this actor was amazing just a little too violent (i.e. hitting a man 5 times in the head with a baseball bat and making me watch the whoooole thing).
+3) The Talented Mr. Ripley: ooooh spooky scary, but when the actors are young and hot it is not a horror movie. Call me by your name, and then I will also call myself by your name.
+4) Nirvana the Band the Show the Movie: I love the Canadians and I spent a lot of time here wondering how they made this. It’s dry and funny and sweet. Might have been overhyped for me but it was good. Wonder what 2008 was like.
+5) Coyote vs ACME: What if Sisyphus enjoyed pushing the boulder but also the boulder considered him a friend? Never watched looney tunes but this felt biblical. Meep meep.
+
+Hope everyone enjoyed their time reading my haphazard and over-comma-ed inaugural guest column. 
+Welcome, get comfortable, this is Bella’s website now baby",
+  author: "Bella Macakanja",
+  date: "10 Sep 2026",
+  title: "AMC and the City",
   paragraphs: [
     `First paragraph. Apostrophes work normally here.`,
     `Second paragraph.`
